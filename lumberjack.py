05aeadc98f5ce40a593f67584d84c5dfcd9b8b23
Andrea Magana <andreamagana1@gmail.com>
2017-10-09 22:20:18 -0500

    Add files via upload
    created functions in treehouse

---
+++ lumberjack.py
@@ -0,0 +1,6 @@
+def lumberjack(name,pronoun):
+    print("{}'s a lumberjack and {} ok!".format(name, pronoun))
+
+lumberjack ("Andrea", "she's")
+lumberjack ("Alex", "he's")
+lumberjack ("Jazzy", "they're")
